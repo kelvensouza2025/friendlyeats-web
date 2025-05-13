274c94a2d4a5fcdef60dfc9ed6bc39b0eee10d59
Kelven Souza <kelven.souza.2025@gmail.com>
2025-05-13 16:30:47 -0300

    refactor(config): remove boilerplate

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,22 +1,10 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { provideAuth, getAuth, connectAuthEmulator } from '@angular/fire/auth';
-import {
-  provideFirestore,
-  getFirestore,
-  connectFirestoreEmulator,
-} from '@angular/fire/firestore';
-import {
-  provideFunctions,
-  getFunctions,
-  connectFunctionsEmulator,
-} from '@angular/fire/functions';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { provideFunctions, getFunctions } from '@angular/fire/functions';
 import { provideMessaging, getMessaging } from '@angular/fire/messaging';
-import {
-  provideStorage,
-  getStorage,
-  connectStorageEmulator,
-} from '@angular/fire/storage';
+import { provideStorage, getStorage } from '@angular/fire/storage';
 import { routes } from './app.routes';
 import { provideRouter } from '@angular/router';
 import { firebaseConfig } from 'src/environments/environment';
@@ -24,26 +12,11 @@ import { firebaseConfig } from 'src/environments/environment';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideFirebaseApp(() => initializeApp(firebaseConfig)),
-    provideFirestore(() => getFirestore()),
     provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
     provideFunctions(() => getFunctions()),
     provideStorage(() => getStorage()),
     provideMessaging(() => getMessaging()),
     provideRouter(routes),
-    provideFirebaseApp(() =>
-      initializeApp({
-        projectId: 'friendlychat-kelven',
-        appId: '1:462191980426:web:5df1c3d777438fa64aa273',
-        storageBucket: 'friendlychat-kelven.firebasestorage.app',
-        apiKey: 'AIzaSyAlc3ebgKmN9M0eQruyGn9rglzZ9J4XrYA',
-        authDomain: 'friendlychat-kelven.firebaseapp.com',
-        messagingSenderId: '462191980426',
-        measurementId: 'G-Z923SJY06H',
-      })
-    ),
-    provideAuth(() => getAuth()),
-    provideFirestore(() => getFirestore()),
-    provideMessaging(() => getMessaging()),
-    provideStorage(() => getStorage()),
   ],
 };
