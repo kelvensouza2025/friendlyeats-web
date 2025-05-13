822daca623672662cd6071579e62714271543d9c
Kelven Souza <kelven.souza.2025@gmail.com>
2025-05-13 15:32:52 -0300

    setup(providers): unwrapp providers

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,23 +2,48 @@ import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
 import { provideAuth, getAuth, connectAuthEmulator } from '@angular/fire/auth';
-import { provideFirestore, getFirestore, connectFirestoreEmulator } from '@angular/fire/firestore';
-import { provideFunctions, getFunctions, connectFunctionsEmulator} from '@angular/fire/functions';
+import {
+  provideFirestore,
+  getFirestore,
+  connectFirestoreEmulator,
+} from '@angular/fire/firestore';
+import {
+  provideFunctions,
+  getFunctions,
+  connectFunctionsEmulator,
+} from '@angular/fire/functions';
 import { provideMessaging, getMessaging } from '@angular/fire/messaging';
-import { provideStorage, getStorage, connectStorageEmulator } from '@angular/fire/storage';
+import {
+  provideStorage,
+  getStorage,
+  connectStorageEmulator,
+} from '@angular/fire/storage';
 import { routes } from './app.routes';
 import { provideRouter } from '@angular/router';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    importProvidersFrom(
-      provideFirebaseApp(() => initializeApp(environment.firebase)),
-      provideFirestore(() => getFirestore()),
-      provideAuth(() => getAuth()),
-      provideFunctions(() => getFunctions()),
-      provideStorage(() => getStorage()),
-      provideMessaging(() => getMessaging())
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore()),
+    provideAuth(() => getAuth()),
+    provideFunctions(() => getFunctions()),
+    provideStorage(() => getStorage()),
+    provideMessaging(() => getMessaging()),
+    provideRouter(routes),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'friendlychat-kelven',
+        appId: '1:462191980426:web:5df1c3d777438fa64aa273',
+        storageBucket: 'friendlychat-kelven.firebasestorage.app',
+        apiKey: 'AIzaSyAlc3ebgKmN9M0eQruyGn9rglzZ9J4XrYA',
+        authDomain: 'friendlychat-kelven.firebaseapp.com',
+        messagingSenderId: '462191980426',
+        measurementId: 'G-Z923SJY06H',
+      })
     ),
-    provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"friendlychat-kelven","appId":"1:462191980426:web:5df1c3d777438fa64aa273","storageBucket":"friendlychat-kelven.firebasestorage.app","apiKey":"AIzaSyAlc3ebgKmN9M0eQruyGn9rglzZ9J4XrYA","authDomain":"friendlychat-kelven.firebaseapp.com","messagingSenderId":"462191980426","measurementId":"G-Z923SJY06H"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage())
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideMessaging(() => getMessaging()),
+    provideStorage(() => getStorage()),
   ],
 };
