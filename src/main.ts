76a4dee29414e149c438b8632bb42904adbbff2e
Kelven Souza <kelven.souza.2025@gmail.com>
2025-05-14 13:38:38 -0300

    feat(worker): add service worker to main file

--- src/main.ts
+++ src/main.ts
@@ -5,3 +5,15 @@ import { appConfig } from './app/app.config';
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err)
 );
+
+// 🔽 Registra o Service Worker do Firebase Messaging
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('firebase-messaging-sw.js')
+    .then((registration) => {
+      console.log('Service Worker registrado com sucesso:', registration);
+    })
+    .catch((err) => {
+      console.error('Erro ao registrar o Service Worker:', err);
+    });
+}
